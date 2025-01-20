dae14ae3f280bbfba03dbd083ec44255ee2fb9ca
johnclaytonmangum@gmail.com <johnclaytonmangum@gmail.com>
2025-01-20 18:18:07 -0500

    Add project files.

---
+++ Models/Jokes.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+
+namespace DailyJokesApp.Models
+{
+    public class Jokes
+    {
+        [Key]
+        public int JokeId { get; set; }
+        [Required]
+        public string Question { get; set; }
+        public string Username { get; set; }
+        [Required]
+        public string Answer { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string DateCreated { get; set; }
+        public int NumberOfLikes { get; set; }
+    }
+}
---
+++ Models/SampleDBContext.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DailyJokesApp.Models
+{
+    public partial class SampleDBContext : DbContext
+    {
+        public SampleDBContext(DbContextOptions
+            <SampleDBContext> options) : base(options)
+        {
+        }
+        //The name of the table "Jokes" needs to match the name of our models table
+        public virtual DbSet<Jokes> Jokes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Jokes>(entity =>
+            {
+                //We are putting in what column in the table is the key column
+                entity.HasKey(k => k.JokeId);
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
